0192acf0f50417073a2a2e5daddc916713c1a346
lowpez <lopezaguayo@aol.com>
2021-09-16 23:13:02 -0300

    Extras de Unidad 2

---
+++ sketches/sketch-01.js
@@ -0,0 +1,58 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, width, height);
+    context.lineWidth = width * 0.01;
+
+    /*
+    const w = 60;
+    const h = 60;
+    const gap = 20;
+    let x, y;*/
+
+    /*Refactorizado
+    Para mas flexibilidad estos valores pueden ser porcentuales*/
+    const w = width * 0.10;
+    const h = height * 0.10;
+    const gap = width * 0.03;
+    //Initial X and Initial Y
+    const ix = width * 0.17;
+    const iy = height * 0.17;
+
+    const off = width * 0.02;
+
+    let x, y;
+
+    for(let i = 0; i < 5; i++){
+      for(let j = 0; j < 5; j++){
+
+        /*x = 100 + (w + gap) * i;
+        y = 100 + (h + gap) * j;*/
+        
+        x = ix + (w + gap) * i;
+        y = iy + (h + gap) * j;
+  
+        context.beginPath();
+        context.rect(x, y , w, h);
+        context.stroke();
+        context.strokeStyle = "white";
+
+        if(Math.random() > 0.5){ 
+            context.beginPath();
+            //context.rect(x + 8, y + 8, w - 16, h - 16);
+            context.rect(x + off / 2, y + off / 2, w - off, h - off);
+            context.stroke();
+        }
+      }
+    }
+
+  };
+};
+
+canvasSketch(sketch, settings);
